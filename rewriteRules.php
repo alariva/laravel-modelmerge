fc3d211317d210a26d61d4ca93095d29f44159b3
Ariel Vallese <alariva@gmail.com>
2018-09-10 11:58:03 -0300

    Remove unused files

--- rewriteRules.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Available placeholders: Alariva, ModelMerge, alariva, modelmerge
-return [
-    'src/MyPackage.php' => 'src/ModelMerge.php',
-    'config/mypackage.php' => 'config/modelmerge.php',
-    'src/Facades/MyPackage.php' => 'src/Facades/ModelMerge.php',
-    'src/MyPackageServiceProvider.php' => 'src/ModelMergeServiceProvider.php',
-];
